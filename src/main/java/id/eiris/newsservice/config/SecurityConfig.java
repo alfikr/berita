76e5541042749a39a3cadf811254882b68e73c3b
alfikr <awal.baru77@gmail.com>
2019-06-19 04:02:21 +0700

    configure webflux security

--- src/main/java/id/eiris/newsservice/config/SecurityConfig.java
+++ src/main/java/id/eiris/newsservice/config/SecurityConfig.java
@@ -4,21 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.session.SessionManagementFilter;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        return http
                 .csrf()
                 .disable()
-                .authorizeRequests()
-                .antMatchers(("/login"))
-                .permitAll()
-                .anyRequest()
-                .authenticated();
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .build();
     }
 }

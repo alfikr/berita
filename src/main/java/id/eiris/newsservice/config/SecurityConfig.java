0e026f6052126ce0edbee366b1b1cc194a40f07a
alfikr <awal.baru77@gmail.com>
2019-06-19 03:38:02 +0700

    add security config

---
+++ src/main/java/id/eiris/newsservice/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package id.eiris.newsservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(("/login"))
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
